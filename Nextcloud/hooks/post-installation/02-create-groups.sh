882aba45c4a5cc7e3d34d9a88c74d4e724b4ec6c
Nicolas Mahé <nicolas.mahe33@gmail.com>
2025-09-03 17:39:02 +0200

    nextcloud | hooks : groups add default admin in groups

--- Nextcloud/hooks/post-installation/02-create-groups.sh
+++ Nextcloud/hooks/post-installation/02-create-groups.sh
@@ -2,12 +2,26 @@
 set -e
 
 ALLOWED_GROUP="${NEXTCLOUD_EXTERNAL_GROUP:-admin}"
+ADMIN_USER="${NEXTCLOUD_ADMIN_USER:-admin}"
 
-# Creer le groupe si il n'existe pas
+# Création du groupe s'il n'existe pas
 if ! php occ group:list | grep -q "$ALLOWED_GROUP"; then
   echo "➕ Création du groupe '$ALLOWED_GROUP'..."
   php occ group:add "$ALLOWED_GROUP"
 else
   echo "ℹ️ Le groupe '$ALLOWED_GROUP' existe déjà."
 fi
-echo "✅ Tous les groupes sont configurés."
+
+# Ajout de l'utilisateur admin au(x) groupe(s)
+if php occ user:list | grep -q "$ADMIN_USER"; then
+  if php occ group:list-members "$ALLOWED_GROUP" | grep -q "$ADMIN_USER"; then
+    echo "ℹ️ L’utilisateur '$ADMIN_USER' est déjà membre du groupe '$ALLOWED_GROUP'."
+  else
+    echo "👤 Ajout de l’utilisateur '$ADMIN_USER' au groupe '$ALLOWED_GROUP'..."
+    php occ group:adduser "$ALLOWED_GROUP" "$ADMIN_USER"
+  fi
+else
+  echo "⚠️ L’utilisateur '$ADMIN_USER' n’existe pas encore dans Nextcloud, impossible de l’ajouter."
+fi
+
+echo "✅ Configuration des groupes terminée."
