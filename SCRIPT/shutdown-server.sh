87cc519bf2447f57a4b2c63ad43de68ebb5434ff
Nicolas Mahé <nicolas.mahe33@gmail.com>
2026-02-07 14:39:58 +0100

    SCRIPT | add edit shutdown after 1 minute

--- SCRIPT/shutdown-server.sh
+++ SCRIPT/shutdown-server.sh
@@ -16,7 +16,7 @@ if ping -c 2 -W 2 "$REMOTE_HOST" > /dev/null 2>&1; then
     echo "Ping OK."
     echo "Connexion SSH sur ${REMOTE_USER}@${REMOTE_HOST} (port $REMOTE_PORT)..."
     
-    ssh -p "$REMOTE_PORT" -o ConnectTimeout=5 ${REMOTE_USER}@${REMOTE_HOST} "sudo shutdown -h now"
+    ssh -p "$REMOTE_PORT" -o ConnectTimeout=5 ${REMOTE_USER}@${REMOTE_HOST} "sudo shutdown -h 1"
     
     if [ $? -eq 0 ]; then
         echo "Commande d'extinction envoyée avec succès."
